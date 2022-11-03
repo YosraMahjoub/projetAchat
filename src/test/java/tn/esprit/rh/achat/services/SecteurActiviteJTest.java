7af8f97aad2bebea812af5905968e8271f76a201
YosraMahjoub <yosra.mahjoub@esprit.tn>
2022-11-03 22:19:17 +0100

    JunitTest

--- src/test/java/tn/esprit/rh/achat/services/SecteurActiviteJTest.java
+++ src/test/java/tn/esprit/rh/achat/services/SecteurActiviteJTest.java
@@ -12,7 +12,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import tn.esprit.rh.achat.entities.SecteurActivite;
 
-
+@SpringBootTest
 public class SecteurActiviteJTest {
 	@Autowired
 	ISecteurActiviteService activiteService;
