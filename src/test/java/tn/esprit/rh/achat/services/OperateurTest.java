d716cc3a657b9f67a3c6f9c7792e2a5b2bddcc14
MariemArif <mariem.arif@esprit.tn>
2022-11-06 14:06:29 +0100

    first commit

---
+++ src/test/java/tn/esprit/rh/achat/services/OperateurTest.java
@@ -0,0 +1,48 @@
+package tn.esprit.rh.achat.services;
+
+import static org.mockito.Mockito.lenient;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import tn.esprit.rh.achat.entities.Operateur;
+import tn.esprit.rh.achat.repositories.OperateurRepository;
+
+
+@SpringBootTest
+@ExtendWith(MockitoExtension.class)
+public class OperateurTest {
+
+	
+	@Mock
+	
+	OperateurRepository operateurRepository;
+	
+	@InjectMocks
+	OperateurServiceImpl OperateurService;
+
+	
+	Operateur op = new Operateur( 1L,"op" , "lastop" , "f465465df", null );
+	
+
+
+	@Test
+	public void testRetrieveOp() {
+	Mockito.when(operateurRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(op));
+	Operateur op1 = OperateurService.retrieveOperateur(op.getIdOperateur());
+	Assertions.assertNotNull(op1);
+	}
+
+	
+
+}
