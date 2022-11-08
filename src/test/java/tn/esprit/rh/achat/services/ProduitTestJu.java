cdc05756839f2a0cfd86f854b93d13d6a9ba750e
Ghada2021 <ghada.mhadhbi@esprit.tn>
2022-11-08 19:29:54 +0100

    dadaa125

--- src/test/java/tn/esprit/rh/achat/services/ProduitTestJu.java
+++ src/test/java/tn/esprit/rh/achat/services/ProduitTestJu.java
@@ -3,13 +3,18 @@ package tn.esprit.rh.achat.services;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import org.junit.jupiter.api.*;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
 import tn.esprit.rh.achat.entities.Produit;
 import tn.esprit.rh.achat.services.IProduitService;
 
 import java.util.Date;
 import java.util.List;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes =  ProduitTestJu.class)
 public class ProduitTestJu {
 
