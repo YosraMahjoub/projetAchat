b05deb9e74259fd3b6e748236f6f1e1ebd1f93f6
YosraMahjoub <yosra.mahjoub@esprit.tn>
2022-11-08 20:45:10 +0100

    Junit / mock .5

--- src/test/java/tn/esprit/rh/achat/services/SecteurActiviteJTest.java
+++ src/test/java/tn/esprit/rh/achat/services/SecteurActiviteJTest.java
@@ -2,13 +2,15 @@ package tn.esprit.rh.achat.services;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.runner.RunWith;
 import org.junit.jupiter.api.*;
 
 import tn.esprit.rh.achat.entities.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.*;
-
+import org.springframework.test.context.junit4.SpringRunner;
+@RunWith(SpringRunner.class)
 @SpringBootTest(classes =  SecteurActiviteJTest.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class SecteurActiviteJTest {
