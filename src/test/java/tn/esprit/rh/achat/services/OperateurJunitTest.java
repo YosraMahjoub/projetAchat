811f3ac7746b11c0d81dcf0dbfb00312cd9147b6
MariemArif <mariem.arif@esprit.tn>
2022-11-08 15:09:31 +0100

    fifth.3 commit

--- src/test/java/tn/esprit/rh/achat/services/OperateurJunitTest.java
+++ src/test/java/tn/esprit/rh/achat/services/OperateurJunitTest.java
@@ -4,12 +4,17 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import tn.esprit.rh.achat.entities.Operateur;
 
 @SpringBootTest
+
+@RunWith(SpringRunner.class)
+
 public class OperateurJunitTest {
 	@Autowired
 	OperateurServiceImpl Operateurservice;
