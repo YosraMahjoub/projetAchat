d76d984f9ee7a3bc6339b8e09dced7ccd4f52a7c
YosraMahjoub <yosra.mahjoub@esprit.tn>
2022-11-08 22:19:55 +0100

    Junit / mock .8

--- src/test/java/tn/esprit/rh/achat/services/SecteurActiviteJTest.java
+++ src/test/java/tn/esprit/rh/achat/services/SecteurActiviteJTest.java
@@ -4,11 +4,12 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.runner.RunWith;
 import org.junit.jupiter.api.*;
-
+import java.util.List;
 import tn.esprit.rh.achat.entities.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.*;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes =  SecteurActiviteJTest.class)
@@ -20,10 +21,11 @@ import org.springframework.test.context.junit4.SpringRunner;
 	
 	@Test
 	@Order(1)
-	public void testAddsecta() {
-		SecteurActivite activite = new SecteurActivite(5L, "code1", "libelle1", null);
-		SecteurActivite sasaved= activiteService.addSecteurActivite(activite);
-		Assertions.assertNotNull(sasaved);
+	public void testretrsecta() {
+		
+	        List<SecteurActivite> activites = activiteService.retrieveAllSecteurActivite();
+	    	Assertions.assertNotNull(activites);
+	    
 	} 
 
 }
